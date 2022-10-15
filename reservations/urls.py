1ff60cf2fa2c2baaa8fd5646a0e7078177023145
J-EUM <eumjungmin1@gmail.com>
2022-10-15 22:55:06 +0900

    [ADD] 예약수정

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from reservations.views import ReservationView
+
 urlpatterns = [
+    path('', ReservationView.as_view()),
+    path('/<str:reservation_number>', ReservationView.as_view())
 ]
