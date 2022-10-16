79f8126b2c2ff1f1ab2caa69944beb8724641983
melony <hjz1010@gmail.com>
2022-10-16 15:59:25 +0900

    [FIX] 병원 정보에 예약가능/불가능 추가 후 관련 코드 수정

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
+from email.policy import default
 from django.db   import models
 from core.models import TimeStampModel
 
 class User(TimeStampModel): 
     name         = models.CharField(max_length=50)
     contact      = models.CharField(max_length=50, unique=True)
-    is_blacklist = models.BooleanField()
+    is_blacklist = models.BooleanField(default=0)
 
     class Meta: 
         db_table = 'users'
